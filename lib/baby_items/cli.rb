6b81b56bb9192242ec8b9c06bb3539763df6849f
Kelly Hesterly <kelly.hesterly@gmail.com>
2021-03-03 19:58:50 +0000

    Added get_user_category

--- lib/baby_items/cli.rb
+++ lib/baby_items/cli.rb
@@ -2,18 +2,24 @@ class BabyItems::CLI
 
   def call
     puts "\nWelcome to the top-rated baby items list!\n"
-    get_categories
+    categories
+    list_categories
     get_user_category
     #get_category
     #scrape_baby_items for selected (category)
   end
 
-  def get_categories
+  def categories
     @categories = ["Nursing & Feeding", "Lounging", "Hygiene & Health", "Transportation", "Sleep", "Diaper Changing", "Play"]
   end
 
-  def get_user_category
+  def list_categories
     puts "\nPlease select the category you wish to see by selecting the accompany number.\n"
     @categories.each.with_index(1) {|category, index| puts "#{index}. #{category}" }
   end
+
+  def get_user_category
+    chosen_category = gets.strip
+  end
+
 end
