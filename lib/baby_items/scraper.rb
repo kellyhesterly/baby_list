c539423c348087b05939e048f41197ec0011032b
Kelly Hesterly <kelly.hesterly@gmail.com>
2021-03-05 00:03:11 +0000

    Scraping for items once more

--- lib/baby_items/scraper.rb
+++ lib/baby_items/scraper.rb
@@ -2,7 +2,11 @@ class BabyItems::Scraper
 
   def self.scrape_baby_items
     doc = Nokogiri::HTML(open("https://www.babylist.com/hello-baby/best-baby-products"))
-    items = doc.css(".product-title.text-underline-fancy")
-    items.each {|item| puts item.children.text.strip}
+    items = doc.css(".product-description-container").children.css("a").attribute("name").value
+    # items = doc.css(".product-title.text-underline-fancy").children.text.strip.split("\n")
+    # item_list = items.each.with_index(1) {|item, index| puts "#{index}. #{item}"}
+    binding.pry
+    # items = doc.css(".product-title.text-underline-fancy")
+    # items.map {|item| puts item.children.text.strip}
   end
 end
