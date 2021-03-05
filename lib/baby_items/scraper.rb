fe39008049a621d190392fa16d2be98372fb9a7f
Kelly Hesterly <kelly.hesterly@gmail.com>
2021-03-05 00:18:41 +0000

    perfected scraping items

--- lib/baby_items/scraper.rb
+++ lib/baby_items/scraper.rb
@@ -2,11 +2,7 @@ class BabyItems::Scraper
 
   def self.scrape_baby_items
     doc = Nokogiri::HTML(open("https://www.babylist.com/hello-baby/best-baby-products"))
-    items = doc.css(".product-description-container").children.css("a").attribute("name").value
-    # items = doc.css(".product-title.text-underline-fancy").children.text.strip.split("\n")
-    # item_list = items.each.with_index(1) {|item, index| puts "#{index}. #{item}"}
-    binding.pry
-    # items = doc.css(".product-title.text-underline-fancy")
-    # items.map {|item| puts item.children.text.strip}
+    items = doc.css(".product-description-container").children.css("a")
+      items.map {|item| puts item.attr("name")}
   end
 end
