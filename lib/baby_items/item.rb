2155ecb9364d8547d7819f9534c444a79fc1e1e3
Kelly Hesterly <kelly.hesterly@gmail.com>
2021-03-08 23:13:59 +0000

    Added attributes to Item class

--- lib/baby_items/item.rb
+++ lib/baby_items/item.rb
@@ -1,10 +1,15 @@
 class BabyItems::Item
-  attr_accessor :name
+  attr_accessor :name, :price, :description, :stores, :specs, :category
 
   @@all = []
 
   def initialize(name)
     @name = name
+    @price = price
+    @description = description
+    @stores = stores
+    @specs = specs
+    @category = category
     save
   end
 
--- lib/baby_items/scraper.rb
+++ lib/baby_items/scraper.rb
@@ -3,20 +3,18 @@ class BabyItems::Scraper
   def self.scrape_baby_items
     doc = Nokogiri::HTML(open("https://www.babylist.com/hello-baby/best-baby-products"))
 
-    items = doc.css(".product-title.text-underline-fancy")
-    items.each.with_index(1) {|item, index| puts "#{index}. #{item.text.strip}"}
-
-    # sort_categories = doc.css(".mtl p").children.text
-    # sort_categories.map do |item|
-    #   if sort_categories.include?("bottles" || "breast" || "nursing")
-    #       << item
-    #
-    # # binding.pry
-    #   end
-    # end
+    item_names = doc.css(".product-title.text-underline-fancy")
+    item_names.each.with_index(1) {|item, index| puts "#{index}. #{item.text.strip}"}
   end
+
 end
+
+
 
 
 # items = doc.css(".product-description-container").children.css("a")
 #   items.map {|item| puts item.attr("name")}
+
+# sort_by_categories = doc.css(".mtl").children.css("p")
+# sort_by_categories.map do |item|
+#   if item.text.include?("bottles" || "breast" || "nursing")
