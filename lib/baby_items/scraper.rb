7a8f3ec0e8a6e29743c98f40b8f2fb3cb1ad4ae2
Kelly Hesterly <kelly.hesterly@gmail.com>
2021-03-08 19:53:56 +0000

    Added each._with_index to scrape_baby_items

--- lib/baby_items/scraper.rb
+++ lib/baby_items/scraper.rb
@@ -2,7 +2,13 @@ class BabyItems::Scraper
 
   def self.scrape_baby_items
     doc = Nokogiri::HTML(open("https://www.babylist.com/hello-baby/best-baby-products"))
-    items = doc.css(".product-description-container").children.css("a")
-      items.map {|item| puts item.attr("name")}
+
+    items = doc.css(".product-title.text-underline-fancy")
+    items.each.with_index(1) {|item, index| puts "#{index}. #{item.text.strip}"}
+    # binding.pry
   end
 end
+
+
+# items = doc.css(".product-description-container").children.css("a")
+#   items.map {|item| puts item.attr("name")}
