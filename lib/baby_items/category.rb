6d55a72cfae3e2b234cb074e8205ef287864a9df
Kelly Hesterly <kelly.hesterly@gmail.com>
2021-03-08 23:34:22 +0000

    Added attr_accessor to Category class

--- lib/baby_items/category.rb
+++ lib/baby_items/category.rb
@@ -1,6 +1,6 @@
 class BabyItems::Category
-  attr_accessor :name
-  
+  attr_accessor :name, :items
+
   @@all = ["Nursing & Feeding", "Lounging", "Hygiene & Health", "Transportation", "Sleep", "Diaper Changing", "Play"]
 
   def initialize
