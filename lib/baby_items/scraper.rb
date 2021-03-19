0c9d4b10a4a300dab380b533eaa2f073e8b90e65
Kelly Hesterly <kelly.hesterly@gmail.com>
2021-03-19 20:19:26 +0000

    Fixed description for Scraper class

--- lib/baby_items/scraper.rb
+++ lib/baby_items/scraper.rb
@@ -8,12 +8,12 @@ class BabyItems::Scraper
         price = card.css(".product-price.mtm.mbl.h6").children[0].text
         url = card.css("a")[0].attributes["href"].value
         stores = card.css(".offer-store").children.map {|store| store.text.strip}
-        description = card.css(".mtl p").children[0].text.strip
+        description = card.css(".mtl p").children.text.strip
+        # description = card.css(".mtl p").children[0].text.strip
+        # binding.pry
 
         BabyItems::Item.new(name, price, url, stores, description)
 
       end
     end
   end
-
-    
